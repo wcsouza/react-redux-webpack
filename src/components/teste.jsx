6fb6e61d0e16c4a657b626ea11fb36fa81174e93
William Souza <william.souza@shareprime.local>
2018-11-06 15:47:58 -0200

    add input

--- src/components/teste.jsx
+++ src/components/teste.jsx
@@ -3,15 +3,22 @@ import { connect } from 'react-redux';
 import * as action from '../store/actions/testeActions';
 
 class Teste extends Component {
+    state = { inputValue: '' };
+
     handleEvent = event => {
-        console.log(this);
-        this.props.AlterarValor('novo valor');
+        this.props.AlterarValor(this.state.inputValue);
     };
+
+    onChangeInput = (event) => {
+        this.setState({ inputValue: event.target.value });
+    }
 
     render() {
         return (
             <div>
                 <h1>Valor: {this.props.valor}</h1>
+                <input type="text" value={this.state.inputValue} onChange={this.onChangeInput} />
+                <br />
                 <button onClick={this.handleEvent}>Alterar Valor</button>
             </div>
         );
